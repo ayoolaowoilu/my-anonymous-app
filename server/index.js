f5d768abd36deff713d38d365b864e0f54dac4c7
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-03-17 16:01:17 +0100

    initial commit

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ app.use(cors(
     
 }
 ))
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin','X-Requested-With','Content-Type','Accept')
+    res.header('Access-Control-Allow-Methods','GET , POST , PUT , DELETE , OPTIONS')
+    next()
+})
 app.use(express.json())
 app.use("/auth",router)
 app.get("/",(req,res)=>{
